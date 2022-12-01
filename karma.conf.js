ddb8010be988da4fc8f22b1e45e614dcb6436d1c
Aymen Salhi <96130051+aymennsalhi@users.noreply.github.com>
2022-12-01 15:38:02 +0300

    aymen portfolio moved here

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/landing'),
+      dir: require('path').join(__dirname, './coverage/AymenSalhiPortfolio'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  { 
+    path: 'home', 
+    component: HomeLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) 
+      }
+    ]
   },
+  { 
+    path: 'blog', 
+    component: HomeLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) 
+      }
+    ]
+  }, 
+  { 
+    path: 'contact', 
+    component: HomeLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./contact-me/contact-me.module').then(m => m.ContactMeModule)
+      }
+    ]
+  },
+  { path: 'visitor/my-services', loadChildren: () => import('./portfolio/services/services.module').then(m => m.ServicesModule) },
+  { path: 'visitor/my-skills', loadChildren: () => import('./portfolio/skills/skills.module').then(m => m.SkillsModule) },
+  { path: 'visitor/my-works', loadChildren: () => import('./portfolio/works/works.module').then(m => m.WorksModule) },
+  { path: 'visitor/my-feedbacks', loadChildren: () => import('./portfolio/feedbacks/feedbacks.module').then(m => m.FeedbacksModule) },
+  { path: 'visitor/my-education', loadChildren: () => import('./portfolio/education/education.module').then(m => m.EducationModule) },
+  { path: 'visitor/contact-me', loadChildren: () => import('./portfolio/contactme/contactme.module').then(m => m.ContactmeModule) },
+  
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'home'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
