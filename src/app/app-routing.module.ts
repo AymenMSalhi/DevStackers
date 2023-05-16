2954c5e4d21098e92a2281210289bba1cd2de54c
aymensalhii <aymenm.salhi@gmail.com>
2023-05-16 23:25:07 +0300

    new initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', 
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+  },
+  { path: 'projects', loadChildren: () => import('./pages/projects/projects.module').then(m => m.ProjectsModule) },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
